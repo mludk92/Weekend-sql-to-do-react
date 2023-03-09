453891e6471a72f3a2eefa858cfa0c948784447d
Mitch Ludkey <mludk92@hotmail.com>
2023-03-09 15:51:49 -0600

    update db with new column status_Comp, and built GET testing in postman next

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,8 +2,42 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
-// GET
+/**
+ * steps 
+ * --GET will need to pull values from db
+ * which is named weenend-to-do-app --table todo
+ * --Populate DOM with the response keys task, priority_Lev, status_comp
+ * ----write query to pull * from db
+ * 
+ * --POST will need to take values from html input, and 
+ * send back to router, 
+ * --query to insert into db will be required
+ * 
+ * --PUT when a task is completed or updated will need to
+ * alter db by index updating table
+ * update query will be needed. JS must reload GET function
+ * at this point 
+ * 
+ * --Delete using index value of Item from table, which GET has 
+ * writen into the DOM with template literal.
+ * write delete query to remove index item from the table
+ */
 
+
+// GET
+router.get('/',(req,res)=>{
+    console.log('IN GET')
+    //query to get all items from todo table
+    let queryText = `select * from todo`
+    pool.query(queryText).then((result)=>{
+        //sending table rows for use in JS
+        res.send(result.rows)
+    }).catch((error)=>{
+        console.log(`Error in GET ${error}`)
+        res.sendStatus(500)
+        //error message
+    })
+})
 // POST
 
 // PUT
--- todo.sql
+++ todo.sql
@@ -1,8 +1,13 @@
+--drop table todo
+
 CREATE TABLE todo (
 	id SERIAL PRIMARY KEY,
 	task varchar(255),
-	priority varchar(10)
+	priority_Lev varchar(10),
+	status_Comp boolean not null
 );
 
-insert into todo (task, priority)
-values('Create a Todo App', 'High')
+insert into todo (task, priority_Lev, status_Comp)
+values('Create a Todo App', 'High', true)
+
+select * from todo
